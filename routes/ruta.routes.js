0c7cb2da73f4453c36543d8ca0ee48cf57663ff2
tatilai <ramirezlaila.t23@gmail.com>
2025-05-15 22:49:49 -0300

    ++

--- routes/ruta.routes.js
+++ routes/ruta.routes.js
@@ -1,5 +1,5 @@
 // /routes/ruta.routes.js
-import express from 'express';
+/*import express from 'express';
 import * as RutaController from '../controllers/ruta.controller.js';
 
 const router = express.Router();
@@ -9,5 +9,26 @@ router.get('/:id', RutaController.getById);
 router.post('/', RutaController.create);
 router.put('/:id', RutaController.update);
 router.delete('/:id', RutaController.remove);
+
+export default router;*/
+
+
+import { Router } from 'express';
+import Ruta from '../models/ruta.model.js';
+
+const router = Router();
+
+// Obtener todas las rutas
+router.get('/', async (req, res) => {
+  const rutas = await Ruta.find();
+  res.json(rutas);
+});
+
+// Crear nueva ruta
+router.post('/', async (req, res) => {
+  const nuevaRuta = new Ruta(req.body);
+  await nuevaRuta.save();
+  res.status(201).json(nuevaRuta);
+});
 
 export default router;
