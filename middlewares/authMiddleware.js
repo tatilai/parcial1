dd02226514efff20c1ea728dbf2003c31cc1b986
tatilai <ramirezlaila.t23@gmail.com>
2025-05-18 17:01:40 -0300

    jwt en ruta reportes

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,22 @@
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();
+const secretKey = process.env.JWT_SECRET;
+
+const authenticateJWT = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) return res.sendStatus(401); // token no enviado
+
+  const token = authHeader.split(" ")[1]; // extraer solo el token después de "Bearer"
+
+  jwt.verify(token, secretKey, (err, payload) => {
+    if (err) return res.status(403).json({ message: "Token inválido" });
+
+    req.user = payload; // guardamos info del usuario logueado
+    next(); // continuar con la siguiente función
+  });
+};
+
+export default authenticateJWT;
